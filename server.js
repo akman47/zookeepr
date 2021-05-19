8fe3fa8e63e6c1f9c3f61dfde3cc1614bea94e4a
Angela Man <angela.k.man@gmail.com>
2021-05-19 14:46:21 -0500

    add validation to data

--- server.js
+++ server.js
@@ -60,6 +60,22 @@ function createNewAnimal(body, animalsArray) {
     return animal;
 }
 
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -80,15 +96,21 @@ app.get('/api/animals/:id', (req, res) => {
 });
 
 app.post('/api/animals/', (req, res) => {
+    // req.body is where incoming content will be
+    // console.log(req.body);
+    
     // set id based on what the next index of array will be
     req.body.id = animals.length.toString();
 
-    // add animal to json file and animals array
-    const animal = createNewAnimal(req.body, animals);
-
-    // req.body is where incoming content will be
-    console.log(req.body);
-    res.json(animal);
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    }
+    else {
+        // add animal to json file and animals array
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
 });
 
 app.listen(PORT, () => {
